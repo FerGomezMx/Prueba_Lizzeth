de4ad248ad9769bf90c60b90c7f7b4e267c61b97
LIZZETH GOMEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2022-08-25 13:18:18 -0500

    Update

--- prueba_1.R
+++ prueba_1.R
@@ -1,4 +1,26 @@
     
-  # Esto es un intento de Pull Request externo ya con permisos
+    # Esto es un intento de Pull Request externo ya con permisos
     names(mtcars)
-  # Prueba del autor
+  
+    # Pull request con branch y permisos
+    # 1. Modificar codigo
+    names(mtcars)
+    # 2. Creamos una branch desde R
+    # 3. Guardamos cambios y hacemos commit
+    # 4. Hacemos el pull y verifcamos en GitHub
+    # 5. Colocarse en el inicio del repo y seleccionar el nuevo commit
+    # 6. Verificar que el merge se encuentre disponible
+    # 7. Crear el Pull request
+    # 8. Al tener permisos ambos autores pueden hacer el merge
+    # 9. Nuevamente hacen un commit y automaticamente se cierra el pull request
+    # 10. Verificamos con la branch main desde GitHub y R
+    
+    # Nota, si se guardan cambios antes de cambiar el branch, 
+    # hay que agregar commit, pero se sugiere mejor deshacerlos con "revert" o 
+    # si se desean mantener pues hacer nuevamente un pull request
+    # Tambien se pueden mantener haciendo el revert y rechazando el reload (pero no se recomienda)
+    
+    
+    # Ahora hacemos un cambio con rama del autor
+    
+    
