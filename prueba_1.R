280cfa0cc5e652d5acb94ba5d0d422d8efabe93d
LIZZETH GOMEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2022-08-25 12:48:06 -0500

    Update de Branch

--- prueba_1.R
+++ prueba_1.R
@@ -1,4 +1,9 @@
     
-  # Esto es un intento de Pull Request externo ya con permisos
+    # Esto es un intento de Pull Request externo ya con permisos
     names(mtcars)
-  # Prueba del autor
+  
+    # Pull request con branch y permisos
+    # 1. Modificar codigo
+    names(mtcars)
+    # 2. Creamos una branch desde R
+    # 3. Guardamos cambios y hacemos commit
